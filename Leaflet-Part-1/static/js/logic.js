b598f448f92bb5aabdaed73bdf1f4435cedc2f95
Aaron Rodriquez <aarononward@gmail.com>
2023-08-29 17:47:38 -0700

    adding colors and popups to circles

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -33,70 +33,11 @@ let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         let div = L.DomUtil.create("div", "legend");
         return div;
     }
-
-
+    info.addTo(map)
 
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson").then(function(earthquakeData) {
     let earthquakes = earthquakeData.features
-    // let earthquake= earthquakes[i]
-    // let coordinates = earthquake.geometry.coordinates
-    // let latitude = coordinates[0]
-    // let longitude = coordinates[1]
-    // let depth = coordinates[2]
-    // let magnitude = earthquake.properties.mag
-
-    //console.log(earthquake.length)
-    // let ranges = {
-    //     less_than_10:[],
-    //     btwn_10_n_30 : [],
-    //     btwn_30_n_50:[],
-    //     btwn_50_n_70 : [],
-    //     btwn_70_n_90:[],
-    //     greater_than_90:[]
-    // }
-    // let icons  = {
-    //     less_than_10: 
-    //             L.circle([latitude,longitude], {
-    //             color: "green",
-    //             fillOpacity : 0.5,
-    //             radius: (magnitude*100)
-    //     }),
-        
-    //     btwn_10_n_30 :
-    //         L.circle([latitude,longitude], {
-    //         color: "yellow-green",
-    //         fillOpacity : 0.5,
-    //         radius: (magnitude*100)
-    //     }),
-
-    //     btwn_30_n_50 : 
-    //         L.circle([latitude,longitude], {
-    //         color: "yellow-orange",
-    //         fillOpacity : 0.5,
-    //         radius: (magnitude*100)
-    //     }),
-
-    //     btwn_50_n_70 :
-    //         L.circle([latitude,longitude], {
-    //         color: "orange",
-    //         fillOpacity : 0.5,
-    //         radius: (magnitude*100)
-    //     }),
-
-    //     btwn_70_n_90 : 
-    //         L.circle([latitude,longitude], {
-    //         color: "dark orange",
-    //         fillOpacity : 0.5,
-    //         radius: (magnitude*100)
-    //     }),
-
-    //     greater_than_90 :
-    //         L.circle([latitude,longitude], {
-    //         color: "red",
-    //         fillOpacity : 0.5,
-    //         radius: (magnitude*100)
-    //     })
-    //     }
+   
     let depthRange
     for (let i=0; i<  earthquakes.length; i++){    
         let earthquake= earthquakes[i]
@@ -108,50 +49,62 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geo
         if (depth < 10) {
             depthRange = "<10",
             L.circle([latitude,longitude], {
-                color: "green",
+                color: "#00CC00",
                 fillOpacity : 0.5,
                 radius: (magnitude*50000)
-            }).addTo(map);
+            }).bindPopup(`<h3>Location : [${latitude} ,  ${longitude}]</h3><hr> 
+            <p>Magnitude : ${magnitude}</p><hr>
+            <p>Depth: ${depth}</p>`).addTo(map);
         }
         else if (depth < 30) {
             depthRange = "10-30",
             L.circle([latitude,longitude], {
-                color: "yellow",
+                color: "#FFFF33",
                 fillOpacity : 0.5,
                 radius: (magnitude*50000)
-            }).addTo(map);
+            }).bindPopup(`<h3>Location : [${latitude} ,  ${longitude}]</h3><hr> 
+            <p>Magnitude : ${magnitude}</p><hr>
+            <p>Depth: ${depth}</p>`).addTo(map);
         }
         else if (depth < 50) {
             depthRange = "30-50",
             L.circle([latitude,longitude], {
-                color: "orange",
+                color: "#FFC300",
                 fillOpacity : 0.5,
                 radius: (magnitude*50000)
-            }).addTo(map);
+            }).bindPopup(`<h3>Location : [${latitude} ,  ${longitude}]</h3><hr> 
+            <p>Magnitude : ${magnitude}</p><hr>
+            <p>Depth: ${depth}</p>`).addTo(map);
         }
         else if (depth < 70) {
             depthRange = "50-70",
             L.circle([latitude,longitude], {
-                color: "lightred",
+                color: "#FF5733",
                 fillOpacity : 0.5,
                 radius: (magnitude*50000)
-            }).addTo(map);
+            }).bindPopup(`<h3>Location : [${latitude} ,  ${longitude}]</h3><hr> 
+            <p>Magnitude : ${magnitude}</p><hr>
+            <p>Depth: ${depth}</p>`).addTo(map);
         }
         else if (depth < 90) {
             depthRange = "70-90",
             L.circle([latitude,longitude], {
-                color: "red",
+                color: "#FF3300",
                 fillOpacity : 0.5,
                 radius: (magnitude*50000)
-            }).addTo(map);
+            }).bindPopup(`<h3>Location : [${latitude} ,  ${longitude}]</h3><hr> 
+            <p>Magnitude : ${magnitude}</p><hr>
+            <p>Depth: ${depth}</p>`).addTo(map);
         }
         else if (depth > 90) {
             depthRange = "90+",
             L.circle([latitude,longitude], {
-            color: "darkred",
+            color: "#990000",
             fillOpacity : 0.5,
             radius: (magnitude*50000)
-            }).addTo(map);
+            }).bindPopup(`<h3>Location : [${latitude} ,  ${longitude}]</h3><hr> 
+            <p>Magnitude : ${magnitude}</p><hr>
+            <p>Depth: ${depth}</p>`).addTo(map);
         }
     };
         
@@ -220,14 +173,3 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geo
 
 // info.addTo(myMap)
 
-// let incedents = data.features
-// for (let i = 0; i < incedents.length; i++) {
-//     let icon = L.icon({
-//         iconSize : feature.properties.mag,
-//         markerColor : feature.geometry.coordinates[2]
-//     })
-//     icon.bindPopup(`<h3>Location : [${feature.geometry.coordinates[1]} ,  ${feature.geometry.coordinates[0]}]</h3><hr>
-//     <p>Date: ${new Date(feature.properties.time)}</p><hr>
-//     <p>Magnitude : ${feature.properties.mag}</p><hr>
-//     <p>Depth: ${feature.geometry.coordinates[2]}</p>`);
-//}
